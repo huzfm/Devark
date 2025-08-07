f07deac93c5289bd9b798aa164dd82e3758a6b1b
Huzaif Mushtaq <huzaaifmushtaq@gmail.com>
2025-08-07 12:54:45 +0530

    oauth package fixed

--- packages/oauth/install.js
+++ packages/oauth/install.js
@@ -17,7 +17,7 @@ function detectPackageManager(targetPath) {
       return null
 }
 
-export default async function install(targetPath) {
+export default async function install(targetPath = process.cwd()) {
       targetPath = path.resolve(targetPath)
 
       const packageJsonPath = path.join(targetPath, 'package.json')
