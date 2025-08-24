10e11a9979158337420bda8b2aa48fdcde142139
Huzaif Mushtaq <huzaaifmushtaq@gmail.com>
2025-08-24 14:20:42 +0530

    prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -3,25 +3,35 @@ import eslintPluginNode from 'eslint-plugin-n';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-      {
-            files: ['**/*.{js,ts,tsx,mjs,cjs}'],
-            ignores: ['node_modules/**', 'dist/**', 'build/**'],
-            languageOptions: {
-                  ecmaVersion: 'latest',
-                  sourceType: 'module',
-            },
-            plugins: {
-                  n: eslintPluginNode,
-            },
-            rules: {
-                  'n/no-unsupported-features/es-syntax': 'off',
-                  quotes: [
-                        'error',
-                        'single',
-                        { avoidEscape: true, allowTemplateLiterals: true },
-                  ],
-                  indent: ['warn', 2],
-            },
-      },
-      eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.{js,ts,tsx,mjs,cjs}'],
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      n: eslintPluginNode,
+    },
+    rules: {
+      'n/no-unsupported-features/es-syntax': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+          printWidth: 80,
+        },
+      ],
+      quotes: [
+        'error',
+        'single',
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      indent: ['warn', 2],
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
