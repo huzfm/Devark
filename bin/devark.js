6f3310f8aadd6d95b9c311d74eca435d98d457dd
Huzaif Mushtaq <huzaaifmushtaq@gmail.com>
2025-10-21 11:56:02 +0530

    refactor: rename 'packages' directory to 'modules' for clarity

--- bin/devark.js
+++ bin/devark.js
@@ -5,11 +5,11 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { readFileSync } from 'fs';
 import path from 'path';
-import googleAuth from '../packages/google-oauth/install.js'
-import addOtp from '../packages/resend-otp/install.js';
-import addGithubOAuth from '../packages/github-oauth/install.js';
-import nodemongo from '../packages/node-mongodb-template/install.js';
-import nodepostgres from '../packages/node-postgress-template/install.js';
+import googleAuth from '../modules/google-oauth/install.js'
+import addOtp from '../modules/resend-otp/install.js';
+import addGithubOAuth from '../modules/github-oauth/install.js';
+import nodemongo from '../modules/node-mongodb-template/install.js';
+import nodepostgres from '../modules/node-postgress-template/install.js';
 import { showDevarkLogo } from '../utils/logo.js';
 
 
